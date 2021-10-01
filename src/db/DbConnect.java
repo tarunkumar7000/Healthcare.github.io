9d3416dd4b1bff81dfe78d3bfe7247defba5aff1
tarunkumar7000 <41991408+tarunkumar7000@users.noreply.github.com>
2021-10-01 11:13:24 +0530

    Added New Files

---
+++ src/db/DbConnect.java
@@ -0,0 +1,36 @@
+
+package db;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class DbConnect {
+    public static Connection c;
+    public static Statement st;
+static{
+ try{
+     Class.forName("com.mysql.cj.jdbc.Driver");
+     c=DriverManager.getConnection("jdbc:mysql://localhost:3306/toymart","root","Tarun@123");
+     st=c.createStatement();
+     
+ }catch(Exception ex){
+     JOptionPane.showMessageDialog(null, ex);
+ }
+ 
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
